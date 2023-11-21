fe8add109337f76d406e1699138b070a81b4b10c
Ellouzlab <137563551+Ellouzlab@users.noreply.github.com>
2023-11-21 10:17:23 -0500

    Add files via upload

--- Plasmid_recon.py
+++ Plasmid_recon.py
@@ -1,4 +1,5 @@
-import argparse, shutil
+#!/usr/bin/python3
+import argparse, shutil, traceback
 from utils import *
 
 
@@ -9,11 +10,13 @@ def arguments():
     parser.add_argument('-p', '--plasdb', default="plasmid_db/clust_map", type=str, help="DB containing plasmid fasta")
     parser.add_argument('-c', '--chromdb', default="chromdb/chromdb", type=str, help="DB containing chromosome fasta")
     parser.add_argument('-r', '--repdb', default='rep_db/rep_dna', type=str, help="DB containing ORI fasta")
-    parser.add_argument('--tempdir', default="tmp", type=str, help="Directory to contain temporary files.")
+    parser.add_argument('--tempdir', type=str, help="Directory to contain temporary files.")
     parser.add_argument('-t', '--threads', default=1, type=int, help="Number of threads for blast searches.")
     parser.add_argument('-o', '--outdir', default="output", type=str, help="output directory")
-
+    parser.add_argument('--keep', action='store_true',help="If used, temporary files not deleted")
     args = parser.parse_args()
+    if args.tempdir is None:
+        args.tempdir = f"{args.outdir}/tmp"
     return args
 
 def isplasmid(plas_df, chrom_df):
@@ -32,6 +35,18 @@ def isplasmid(plas_df, chrom_df):
     else:
         return False
 
+def remove_overlap(df):
+    df = df.sort_values(by=['qstart', 'bitscore'], ascending=[True, False])
+    to_remove = set()
+    last_qend = -1
+    for i, row in df.iterrows():
+        if row['qstart'] <= last_qend:
+            to_remove.add(i)
+        else:
+            last_qend = row['qend']
+    df = df.loc[~df.index.isin(to_remove)]
+
+    return df
 def contig_class(contigs_loc, blast_plas_loc, blast_chrom_loc, blast_rep_loc, plasdb, chromdb, repdb ,threads):
     contig_data_arr=[]
     for contig in os.listdir(contigs_loc):
@@ -48,25 +63,32 @@ def contig_class(contigs_loc, blast_plas_loc, blast_chrom_loc, blast_rep_loc, pl
         chrom_df.to_csv(blast_chrom_path)
 
         rep_df=blast_run(contig_path, repdb, threads=threads)
-        rep_df.to_csv(blast_rep_path)
 
         plas_result=isplasmid(plas_df, chrom_df)
 
         #print(contig_path)
         try:
-            rep=True
-            rep_id=rep_df.iloc[0]['qtitle'].split('|')[-1]
+            if not len(rep_df)==0:
+                rep = True
+                filtered_rep_df= rep_df[rep_df['sseqcov']>0.2]
+                filtered_rep_df.loc[filtered_rep_df["sseqcov"] < 0.8, 'sseqid'] = filtered_rep_df['sseqid'] + '_like'
+                filtered_rep_df=remove_overlap(filtered_rep_df)
+                filtered_rep_df.to_csv(blast_rep_path)
+                unique_values = filtered_rep_df['sseqid'].str.split('|').apply(lambda x: x[-1]).unique()
+                rep_id='&'.join(unique_values)
+            else:
+                rep=False
+                rep_id=False
         except:
             rep=False
             rep_id=None
 
         try:
-            idx_max_value = plas_df['Match_base_pid'].idxmax()
-
-            # Retrieve the corresponding title
-            cluster = plas_df.loc[idx_max_value, 'qtitle']
+            idx_max_value = plas_df['bitscore'].idxmax()
+            cluster = plas_df.loc[idx_max_value, 'sseqid']
         except:
             cluster=None
+
         #print(cluster)
 
         contig_data_arr.append((contig_path, chrom_df, plas_df, plas_result, rep, cluster, rep_id))
@@ -81,6 +103,15 @@ def write_chrom_fasta(contig_arr, location):
     if not len(chromosome_records)==0:
         SeqIO.write(chromosome_records, location, "fasta")
 
+def similar_bitscore(score1, score2, threshold):
+    return abs(score1 - score2) / score1 < threshold
+
+def get_similar_hits(df, threshold=0.01):
+
+    highest_score = df["Match_base_pid"].max()
+    similar_seq_ids = df[df['Match_base_pid'].apply(lambda x: similar_bitscore(x, highest_score, threshold))]['sseqid'].tolist()
+    return [similar_seq_ids]
+
 
 def write_plas_fasta(contig_info, outdir):
     plasmid_arr=[]
@@ -93,15 +124,31 @@ def write_plas_fasta(contig_info, outdir):
     prim_clust_dict={}
     unclustered=[]
     for contig in plasmid_arr:
-        clust=contig[5]
-
-        if contig[4]:
-            if clust not in prim_clust_dict:
-                prim_clust_dict[clust]=[contig]
+        plas_ids=get_similar_hits(contig[2])[0]
+        if len(plas_ids)==1:
+            clust=plas_ids[0]
+            if contig[4]:
+                if clust not in prim_clust_dict:
+                    prim_clust_dict[clust]=[contig]
+                else:
+                    prim_clust_dict[clust].append(contig)
             else:
-                prim_clust_dict[clust].append(contig)
+                unclustered.append(contig)
+
         else:
-            unclustered.append(contig)
+            match=False
+            for clust in plas_ids:
+                if contig[4]:
+                    if clust in prim_clust_dict:
+                        prim_clust_dict[clust].append(contig)
+                        match=True
+                        break
+
+            if match == False:
+                if contig[4]:
+                    prim_clust_dict[contig[5]] = [contig]
+                else:
+                    unclustered.append(contig)
 
 
     final_unclustered=[]
@@ -109,7 +156,7 @@ def write_plas_fasta(contig_info, outdir):
         i=0
         clusters_unc=contig[5].split('_')
         match = False
-        while i<99:
+        while i<9:
 
             for key in prim_clust_dict:
                 clusters_cl=key.split('_')
@@ -131,6 +178,10 @@ def write_plas_fasta(contig_info, outdir):
         pass
     else:
         unclust2fasta(final_unclustered, outdir)
+def except_action(tempdir, outdir, keep_tmp):
+    if not keep_tmp:
+        shutil.rmtree(tempdir)
+    shutil.rmtree(outdir)
 
 def main():
     args=arguments()
@@ -140,24 +191,33 @@ def main():
     tempdir = args.tempdir
     threads=args.threads
     outdir=args.outdir
+    keep_tmp=args.keep
 
-    makedir(tempdir)
     makedir(outdir)
+    tempdir = makedir(tempdir)
+    try:
+        contig_location = makedir(f"{tempdir}/contigs")
+        blast_plas_location = makedir(f"{tempdir}/blast_plas_res")
+        blast_chrom_location = makedir(f"{tempdir}/blast_chrom_res")
+        blast_rep_location = makedir(f"{tempdir}/blast_rep_res")
 
-    contig_location = makedir(f"{tempdir}/contigs")
-    blast_plas_location = makedir(f"{tempdir}/blast_plas_res")
-    blast_chrom_location = makedir(f"{tempdir}/blast_chrom_res")
-    blast_rep_location = makedir(f"{tempdir}/blast_rep_res")
+        fasta_breaker(args.infile, contig_location)
 
-    fasta_breaker(args.infile, contig_location)
-
-    contig_info = contig_class(
-        contig_location, blast_plas_location, blast_chrom_location, blast_rep_location, plasdb, chromdb, repdb, threads
-    )
-    chrom_path = f"{outdir}/chromosome.fasta"
-    write_chrom_fasta(contig_info, chrom_path)
-    write_plas_fasta(contig_info, outdir)
-    shutil.rmtree(tempdir)
-
+        contig_info = contig_class(
+            contig_location, blast_plas_location, blast_chrom_location, blast_rep_location, plasdb, chromdb, repdb, threads
+        )
+        chrom_path = f"{outdir}/chromosome.fasta"
+        write_chrom_fasta(contig_info, chrom_path)
+        write_plas_fasta(contig_info, outdir)
+        if not keep_tmp:
+            shutil.rmtree(tempdir)
+    except KeyboardInterrupt:
+        print("\nprogram was interrupted")
+        except_action(tempdir, outdir, keep_tmp)
+    except Exception:
+        print(f"\nAn error occured:\n")
+        traceback.print_exc()
+        except_action(tempdir, outdir, keep_tmp)
 
 main()
+
